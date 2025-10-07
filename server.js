aac67d2bd6affa6110c4647787400dc7a9b5166a
Vijayant Pandey <119390705+vijayant-pandey@users.noreply.github.com>
2025-10-08 02:46:00 +0530

    Update server.js
    replace
    // app.use(cors());
    
    with---
    app.use(cors({
      origin: [process.env.CLIENT_URL],
      credentials: true
    }));

--- server.js
+++ server.js
@@ -25,7 +25,13 @@ const PORT = process.env.PORT || 5000;
 
 // middlewares
 app.use(helmet());
-app.use(cors());
+// app.use(cors());
+
+app.use(cors({
+  origin: [process.env.CLIENT_URL],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
